75434d68450d2c69880b7dc6038bcd4101458c43
Amir Ghezala <mohamedghezala@gmail.com>
2025-04-21 09:42:42 +0400

    add searchform list

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,6 @@
+import H1 from "@/components/h1";
+import { CityEvent } from "@/lib/types";
+
 type CityPageParams = {
   params: Promise<{ city: string }>;
 };
@@ -5,7 +8,27 @@ type CityPageParams = {
 const CityPage = async ({ params }: CityPageParams) => {
   const { city } = await params;
 
-  return <main>{city}</main>;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+
+  const events = await response.json();
+
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px]">
+      <H1 className="mb-28">
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+
+      <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
+        {events.map((event: CityEvent) => (
+          <div key={event.id}>{event.name}</div>
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default CityPage;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./logo";
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+import clsx from "clsx";
 
 const routes = [
   {
@@ -13,17 +18,31 @@ const routes = [
 ];
 
 const Header = () => {
+  const activePathname = usePathname();
+
   return (
     <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <nav>
+      <nav className="h-full">
         <ul className="flex gap-x-6 h-full text-sm">
           {routes.map((route) => (
             <li
               key={route.path}
-              className="text-white/50 hover:text-white flex items-center relative transition"
+              className={clsx(
+                "hover:text-white flex items-center relative transition",
+                {
+                  "text-white": activePathname === route.path,
+                  "text-white/50": activePathname !== route.path,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+              {activePathname === route.path && (
+                <motion.div
+                  layoutId="active-route-underline"
+                  className="bg-accent h-1 w-full absolute bottom-0"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
