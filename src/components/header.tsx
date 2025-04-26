315d16c29cb6e82394b06effbfac16d182ccae61
Amir Ghezala <mohamedghezala@gmail.com>
2025-04-26 08:26:42 +0400

    add util cn function

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Logo from "./logo";
 import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
-import clsx from "clsx";
+import { cn } from "@/lib/utils";
 
 const routes = [
   {
@@ -28,7 +28,7 @@ const Header = () => {
           {routes.map((route) => (
             <li
               key={route.path}
-              className={clsx(
+              className={cn(
                 "hover:text-white flex items-center relative transition",
                 {
                   "text-white": activePathname === route.path,
