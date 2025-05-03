eeac2c4802f2ab6848474fee62ff0966e0c078bf
Amir Ghezala <mohamedghezala@gmail.com>
2025-05-03 14:08:18 +0400

    add loading skeletons to events and event routes

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,5 +1,7 @@
 import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 type CityPageParams = {
   params: Promise<{ city: string }>;
@@ -7,12 +9,6 @@ type CityPageParams = {
 
 const CityPage = async ({ params }: CityPageParams) => {
   const { city } = await params;
-
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
-
-  const events = await response.json();
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px]">
@@ -22,7 +18,9 @@ const CityPage = async ({ params }: CityPageParams) => {
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 };
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -2,13 +2,19 @@ import EventCard from "@/components/event-card";
 import { CityEvent } from "@/lib/types";
 
 type EventsListProps = {
-  events: CityEvent[];
+  city: string;
 };
 
-const EventsList = ({ events }: EventsListProps) => {
+const EventsList = async ({ city }: EventsListProps) => {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+
+  const events = await response.json();
+
   return (
     <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
-      {events.map((event) => (
+      {events.map((event: CityEvent) => (
         <EventCard key={event.id} event={event} />
       ))}
     </section>
